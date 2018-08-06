734546c463a1b0afe52bb8b710a0618f749b6555
sa4dkhan <sa4dkhan@gmail.com>
2018-08-07 00:12:55 +0600

    list update

---
+++ lists.py
@@ -0,0 +1,76 @@
+# Lists
+
+# mylist = [1,2,3]
+
+# mylist = ['string',1,2,3,4,5,True, 'Saad', [1,2,3,4]]
+# print('before reassignment')
+# print(mylist)
+
+# print(mylist)
+
+# print(len(mylist))
+
+# print(mylist[-1])
+
+# print(mylist[1:])
+
+# print(mylist[:3])
+
+# LISTS ARE IMMUTABLE
+# print('after reassignment')
+# mylist[0] = 'Khan'
+# print(mylist)
+
+
+# mylist.append("New Item")
+# mylist.append(['x', 'y', 'z'])
+
+# list_two = ['x', 'y','z']
+# mylist.extend(list_two)
+# print(mylist)
+
+### Remove from the list ###
+# item = mylist.pop(0)
+# print(mylist)
+# print(item)
+
+
+### Reverse method ###
+# mylist.reverse()
+# print(mylist)
+
+
+### Sorting ###
+# mylist = [2, 5, 1, 6, 1, 6, 2, 5, 45, 12, 13]
+# mylist.sort()
+# print(mylist)
+
+
+### Nested list indexing ###
+mylist =  [1,2, ['x', 'y', 'z']]
+# print(mylist[2])
+# print(mylist[2][1])
+
+
+###List comprehensions ###
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]   # list indexing is matrix[0][0]
+
+# first_col = [row[0] for row in matrix]
+# print(first_col)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
