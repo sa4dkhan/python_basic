aa5e5cf5de70355c259c1c72a05ff8856c9fb08d
sa4dkhan <sa4dkhan@gmail.com>
2018-08-06 22:37:00 +0600

    first commit

---
+++ numbers.py
@@ -0,0 +1,11 @@
+print((2+10) * (10+3)) # pemdas = perenthesis, exponenciation, multiplication, division, addition, subtraction
+
+a = 5
+
+print(a+a)
+
+my_income = 100
+tax_rate = 0.1
+my_taxes= my_income * tax_rate
+
+print(my_taxes)
---
+++ strings.py
@@ -0,0 +1,53 @@
+# STRINGS
+
+
+# BASICS
+# mystring = 'abcdefg'
+# print(mystring)
+
+
+# INDEXING
+# mystring = 'abcdefg'
+# print(mystring[0])
+# print(mystring[-1])
+# print(mystring[-2])
+# print(mystring[3])
+
+
+# SLICING
+# mystring = 'abcdefg'
+# print(mystring[2:])
+# print(mystring[4:])
+# print(mystring[:3])
+# print(mystring[2:5])
+# print(mystring[:])
+# print(mystring[::]) #all the way to the beginning and all the way to the end and the number is after is stepsize
+# print(mystring[::1]) # step size
+# print(mystring[::3])
+
+
+# STRINGS ARE IMMUTABLE
+# mystring = 'abcdefg'
+# mystring[0] = 'x'
+# print(mystring)
+
+
+# BASIC METHODS
+# mystring = 'Hello World'
+# x = mystring.upper()
+# x = mystring.capitalize()
+# x = mystring.split('o')
+# print(x)
+
+
+# PRINT FORMATTING
+# x = "Insert another string here: {}".format("INSERT ME!")
+# x = "Item one: {} Item two: {}".format('dog', 'cat')
+x = "Item one: {x} Item two: {y}".format('x= dog', 'y= cat')
+print(x)
+
+
+
+
+
+
