44b0057111f73449b43e3f3b83876213fabdfbf4
Saad Khan <saad.khan@nextdot.com.au>
2018-08-07 14:17:47 +0600

    tuples added

---
+++ tuples_sets_booleans.py
@@ -0,0 +1,32 @@
+### Booleans ###
+
+
+### Tuples ###
+# t = (1,2,3)
+# print(t[0])
+
+
+t = ('a', True, 123)
+# print(t)
+
+### Tuple is not immutable ###
+# t[0] = 'New'
+
+# print(t)
+
+
+### Sets are unorderd and like dictionaries but no key and only takes unique eliment###
+
+# x = set()
+# x.add(1)
+# x.add(2)
+# x.add(4)
+# x.add(0.1)
+# x.add(4)
+# x.add(4)
+
+# print(x)
+
+
+converted = set([1,1,1,1,1,1,1,1,1,1,3,3,3,3,3,3,2,2,2,2,2])
+print(converted)
